4c6fb7c5e420494047ae0e42e7307fb3d14ad5ae
CreateRandom <createrandom1@gmail.com>
2018-01-08 14:54:41 +0100

    Push before rebase.

--- brainfly_quickstart.bat
+++ brainfly_quickstart.bat
@@ -1,6 +1,7 @@
 set batdir=%~dp0
 cd %batdir%
-start .\eeg_quickstart.bat eego %*
+rem start .\eeg_quickstart.bat eego %*
+start .\debug_quickstart.bat
 rem Weird windows hack to sleep for 2 secs to allow the buffer server to start
 ping 127.0.0.1 -n 10 > nul
 
--- matlab/signalProc/train_ersp_clsfr.m
+++ matlab/signalProc/train_ersp_clsfr.m
@@ -91,7 +91,7 @@ opts=struct('classify',1,'fs',[],'timeband_ms',[],'freqband',[],...
             'visualize',1,'badCh',[],'nFold',10,'class_names',[],'zeroLab',1);
 [opts,varargin]=parseOpts(opts,varargin);
 
-disp('In train ersp clsfr')
+disp("In train ersp clsfr")
 
 % get the sampling rate
 if ( isempty(opts.fs) ) error('Sampling rate not specified!'); end;
@@ -117,17 +117,6 @@ if ( isempty(ch_pos) && ~isempty(opts.capFile) && (~isempty(ch_names) || opts.ov
   end
 end
 fs=opts.fs; if ( isempty(fs) ) warning('No sampling rate specified... assuming fs=250'); fs=250; end;
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Print X, Y and channel names for debugging
-disp('Original X (dimensions):')
-size(X)
-disp('Original Y:')
-size(Y)
-disp('Names of original channels')
-ch_names
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %1) Detrend
 if ( opts.detrend )
@@ -183,7 +172,7 @@ if ( opts.badchrm || (~isempty(opts.badCh) && sum(opts.badCh)>0) )
   if( opts.verb>0) fprintf('%d ch removed\n',sum(isbadch)); end;
 end
 
-%3.a) Spatial filter/re-reference (data-dependent-unsupervised)
+              %3.a) Spatial filter/re-reference (data-dependent-unsupervised)
 sftype='';if( ischar(opts.spatialfilter) ) sftype=lower(opts.spatialfilter); end;
 adaptspatialfiltstate=[];
 R=1; % the composed spatial filter to apply.  N.B. R is applied directly as: X = R*X
@@ -229,52 +218,6 @@ if ( size(X,1)>=4 && ...
   if( opts.verb>0) fprintf('\n');end;
 end
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Print X, Y and channel names after bad channel removal
-disp('X (dimensions) after bad channel removal / SLAP:')
-size(X)
-disp('Y after bad channel removal / SLAP:')
-size(Y)
-disp('Names of channels after bad channel removal / SLAP:')
-ch_names
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Remove all non-target channels from X
-
-%Channels to keep:"
-% C3 Cz C4
-targetCh = ["C3", "Cz", "C4"];
-
-% Traverse the list backwards (to avoid troubles when we delete non-target
-% channels), and remove non-target channels.
-for m = numel(ch_names):-1:1
-    if ~any(strcmp(targetCh, ch_names(m)))
-        % remove the channel from X and the ch_names
-        ch_names(m) = [];
-        X(m,:,:) = [];
-    end
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Print X, Y and channel names after non-target channel removal
-disp('X (dimensions) after non-target channel removal:')
-size(X)
-disp('Y  after non-target channel removal:')
-size(Y)
-disp('Names of channels after non-target channel removal:')
-ch_names
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-
 %2.2) time range selection
 timeIdx=[];
 if ( ~isempty(opts.timeband_ms) ) 
@@ -295,9 +238,7 @@ if ( size(X,1)>=4 ) % only spatial filter if enough channels
     ch_pos=[]; 
     % re-name channels
     ch_names={};for ci=1:size(d,1); for clsi=1:size(d,2); ch_names{ci,clsi}=sprintf('SF%d.%d',clsi,ci); end; end;
-    disp('Ch_names renamed after step 3 - spatial filter')
-    ch_names
-    
+
     X=tprod(X,[-1 2 3],Rc,[1 -1]); % filter the data
     R = Rc*R; % compose the combined filter for test time    
   end
@@ -354,7 +295,7 @@ if ( ~isempty(opts.freqband) && ~isempty(fs) )
       else                                freqbands(:,bi)=[mean(opts.freqband{bi}([1 2])) mean(opts.freqband{bi}([3 4]))];
       end
     end
-    disp('freqbands')
+    disp("freqbands")
     freqbands
   else % standardize to band start,end
     if(size(freqbands,1)==1)      freqbands=freqbands'; end;
@@ -380,7 +321,7 @@ if ( opts.timefeat )
   X=cat(2,Xt,X);
   freqs=[0 freqs];
 end
-disp('Freqs:')
+disp("Freqs:")
 freqs
 
 % 5.9) Apply a feature filter post-processor if wanted
@@ -392,19 +333,6 @@ if ( ~isempty(featFiltFn) )
 	 [X(:,:,ei),featFiltState]=feval(featFiltFn{1},X(:,:,ei),featFiltState,featFiltFn{2:end});
   end
 end
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Print X, Y and channel names after freq bin limiting
-disp('X (dimensions) after freq bin limiting:')
-size(X)
-disp('Y  after freq bin limiting:')
-Y
-disp('Names of channels after freq bin limiting:')
-ch_names
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 
 %5.5) Visualise the input?
 aucfig=[];erpfig=[];
